a0cd2d92f6262341a07f14ca30a9c049fdc63655
Youssef Samir <youssefmansour3076y@gmail.com>
2025-07-03 21:06:57 +0300

    Add Common Entities

---
+++ Restaurants.Domains/Common/BaseAuditableEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Domains.Common
+{
+    public class BaseAuditableEntity<TKey> : BaseEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public string CreateBy { get; set; } = null!;
+        public DateTime CreateOn { get; set; }
+        public string ModifiedBy { get; set; } = null!;
+        public DateTime ModifiedOn { get; set; }
+    }
+}
---
+++ Restaurants.Domains/Common/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Domains.Common
+{
+    public class BaseEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        public required TKey Id { get; set; }
+    }
+}
