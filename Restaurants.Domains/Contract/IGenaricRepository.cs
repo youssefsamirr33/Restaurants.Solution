d8aa015a075f830be65e7a088ee5795aeafb663e
Youssef Samir <youssefmansour3076y@gmail.com>
2025-07-12 23:54:33 +0300

    implement the genraic repository

---
+++ Restaurants.Domains/Contract/IGenaricRepository.cs
@@ -0,0 +1,20 @@
+﻿using Restaurants.Domains.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Domains.Contract
+{
+    public interface IGenaricRepository<TEntity , TKey>
+        where TEntity : BaseEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        Task<IReadOnlyList<TEntity>> GetAllAsync(bool WithTracking = false);
+        Task<TEntity?> GetAsync(TKey Id);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);    
+    }
+}
---
+++ Restaurants.Infrastructure.Persistence/Repositories/GenaricRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domains.Common;
+using Restaurants.Domains.Contract;
+using Restaurants.Infrastructure.Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Persistence.Repositories
+{
+    public class GenaricRepository<TEntity, TKey>(RestaurantDbContext _context) : IGenaricRepository<TEntity, TKey>
+        where TEntity : BaseEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+
+
+        public async Task<IReadOnlyList<TEntity>> GetAllAsync(bool WithTracking = false)
+            => WithTracking ? await _context.Set<TEntity>().ToListAsync() : await _context.Set<TEntity>().AsNoTracking().ToListAsync();
+
+        public async Task<TEntity?> GetAsync(TKey Id)
+            => await _context.Set<TEntity>().FindAsync(Id);
+
+        public async Task AddAsync(TEntity entity)
+            => await _context.Set<TEntity>().AddAsync(entity);
+        public void Update(TEntity entity)
+            =>  _context.Set<TEntity>().Update(entity);
+
+        public void Delete(TEntity entity)
+            => _context.Set<TEntity>().Remove(entity);
+
+    }
+}
