086b2aef2055ae8dee4ee0a42cf2db78b4dbfda5
Youssef Samir <youssefmansour3076y@gmail.com>
2025-07-13 08:04:35 +0300

    impelement service manger

--- Restaurants.Application/DependancyInjection.cs
+++ Restaurants.Application/DependancyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Abstraction.Services;
 using Restaurants.Application.Mapping;
+using Restaurants.Application.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,7 @@ namespace Restaurants.Application
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(m => m.AddProfile(new MappingProfile()));
-      
+            services.AddScoped(typeof(IServiceManger) , typeof(ServiceManger));
             return services;
         }
     }
---
+++ Restaurants.Application/Services/ServiceManger.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Restaurants.Abstraction.Services;
+using Restaurants.Domains.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Services
+{
+    public class ServiceManger : IServiceManger
+    {
+        // implement this service with lazy intialization
+        private readonly Lazy<IRestaurantService> _restaurantService;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ServiceManger(IUnitOfWork unitOfWork , IMapper mapper)
+        {
+            _restaurantService = new Lazy<IRestaurantService>(new RestaurantService(_unitOfWork , _mapper));
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public IRestaurantService restaurantService => _restaurantService.Value;
+    }
+}
